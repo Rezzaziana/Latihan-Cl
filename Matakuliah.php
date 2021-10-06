1f3866dc530465033ba6a517faf7ab58e9eb453f
Rezzaziana <12201325@bsi.ac.id>
2021-10-06 21:23:35 +0700

    Matakuliah

--- Matakuliah.php
+++ Matakuliah.php
@@ -10,13 +10,27 @@ class Matakuliah extends Ci_Controller
 
 	public function cetak()
 	{
-		$data = [
+		$this->form_validation->set_rules('kode','Kode matakuliah','required|min_length[3]', [
+				'required' => 'Kode Matakuliah Harus diisi',
+				'min_length' => 'Kode Terlalu Pendek'
+		]);
+
+		$this->form_validation->set_rules('nama','Nama matakuliah','required|min_length[3]', [
+				'required' => 'Nama Matakuliah Harus diisi',
+				'min_length' => 'Nama Terlalu Pendek'
+		]);
+
+		if ($this->form_validation->run() != true) {
+			$this->load->view('view-form-matakuliah');
+		} else {
+			$data = [
 			'kode' => $this->input->post('kode'),
 			'nama' => $this->input->post('nama'),
 			'sks' => $this->input->post('sks')
 		];
 
 		$this->load->view('view-data-matakuliah', $data);
+		}
 	}
 
 }
